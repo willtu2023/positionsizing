43dd3359fea388b61ca172b5c7bf28efa4871374
willtu2023 <132551367+willtu2023@users.noreply.github.com>
2023-05-04 15:38:37 +0800

    Create optimize.py

---
+++ optimize.py
@@ -0,0 +1,10 @@
+import os
+codePath = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.path.append('..')
+
+import numpy as np
+import pandas as pd
+
+import plotly.express as px
+import plotly.graph_objects as go
